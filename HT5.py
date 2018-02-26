f385f50ff8fe9caeb021990ba65247191a2a8c0e
her17138 <her17138@uvg.edu.gt>
2018-02-26 12:06:38 -0600

    final de proceso de peticion de memoria

--- HT5.py
+++ HT5.py
@@ -12,22 +12,16 @@ class Computer:
         self.RAM = simpy.Container(env, init = 0, capacity =100)
         self.CPU = simpy.Resource(env, capacity =1)
         
-def proceso(env, name, computadora):
+def proceso(env, name, arriving_time, computadora):
+    #
+    yield env.timeout(arriving_time)
+    #Momento en el que llega 
+    horaLlegada = env.now
     print "Proceso %s solicita memoria en el momento %s" % (name, env.now)
     cantidadMemoria = random.randint(1, 30)
     print "Proceso %s necesita %d cantidad de memoria." % (name, cantidadMemoria)
-
-    print "Proceso %s esta listo en el momento %s" % (name, env.now)
-    yield computadora.RAM.get(cantidadMemoria)
-    ###
-    memory = computadora.RAM.capacity - computadora.RAM.level
-    yield computadora.RAM.put(memory)
-
-
-
-
-
-
+    yield computadora.RAM.put(cantidadMemoria)
+    print "Memoria otorgada al momento %d al proceso %s. Ha pasado al estado 'Ready' en %s." % (cantidadMemoria, name, env.now)
 
 
 
@@ -40,7 +34,7 @@ env = simpy.Environment() #ambiente de simulacion
 random.seed(10)
 compu = Computer(env)
 for i in range(10):
-    env.process(proceso(env,'carro %d'%i,compu))
+    env.process(proceso(env,'Proceso %d'%i,random.expovariate(1.0/10), compu))
 env.run(50)  #correr la simulacionn hasta el tiempo = 50
 
 #print ("tiempo promedio por vehiculo es: ", totalDia/5.0)
