53e5fa77acec4b00028c9ea5aa7ac783676e4157
her17138 <her17138@uvg.edu.gt>
2018-03-02 17:12:56 -0600

    pulición de método para hacer cola en CPU

--- HT5.py
+++ HT5.py
@@ -24,30 +24,31 @@ def proceso(env, name, numero, arriving_time, computadora):
     cantidadMemoria = random.randint(1, 30)
     print "Proceso %s necesita %d cantidad de memoria." % (name, cantidadMemoria)
     yield computadora.RAM.put(cantidadMemoria)
-    print "Memoria otorgada al momento %d al proceso %s. Ha pasado al estado 'Ready' en %s." % (cantidadMemoria, name, env.now)
+    print "Memoria otorgada al momento %s al proceso %s. Ha pasado al estado 'Ready' en %s." % (env.now, name, env.now)
 
     terminado = False
     while not terminado:
         with computadora.CPU.request() as req:
+            waiting =0
             #Momento para espere a que lo atienda el CPU
-            instrucciones = random.randint(1,10)
-            print "Espera al CPU el proceso %s con %d instrucciones en %s"% (name,instrucciones,env.now)
+            print "Espera al CPU el proceso %s con %d instrucciones en %s"% (name,cantidadMemoria,env.now)
             yield req
-
-            #Inicio de CPU o running
-            print "Proceso %s encuentra atendido por el CPU en %s"% (name,env.now)
-            for i in range(3): #Realiza solamente 3 instrucciones
-                if instrucciones > 0:   #Si llegara a tener operaciones, llega a ejecutar una
-                    instrucciones -= 1
-                    waiting = random.randint(1,2)
+            if instrucciones>3:
+                for i in range(3): #Realiza solamente 3 instrucciones
+                    if instrucciones > 0:   #Si llegara a tener operaciones, llega a ejecutar una
+                        instrucciones -=1
+                        cantidadMemoria -=1
+                waiting = random.randint(1,2)
                 yield env.timeout(1)
             #Finaliza de estar en la CPU
             #Ingresa a cola de Waiting
             if waiting == 1:
                 print "Proceso %s en cola de Waiting para hacer operaciones I/O en %s"% (name,env.now)
-                yield env.timeout(1)
+                yield env.timeout(2)
             #Deja el CPU
             if instrucciones <= 0:
+                computadora.RAM.get(cantidadMemoria)
+            if cantidadMemoria <3:
                 terminado = True
 
     print "Proceso %s ha dejado el CPU en %s"% (name,env.now)
@@ -68,8 +69,8 @@ env = simpy.Environment() #ambiente de simulacion
 #CPU = simpy.Resource(env, capacity = 1)
 random.seed(10)
 compu = Computer(env)
-for i in range(10):
-    env.process(proceso(env,'Proceso %d'%i,i,random.expovariate(1.0/10), compu))
+for i in range(2):
+    env.process(proceso(env,'%d'%i,i,random.expovariate(1.0/10), compu))
 env.run(50)  #correr la simulacionn hasta el tiempo = 50
 tiempopromedio = sum(Tiempoprocesos)*1.0/len(Tiempoprocesos)
 #desvestandar = np.std(Tiempoprocesos)
