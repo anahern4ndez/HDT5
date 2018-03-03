cab2562ace2f0cb9ffb09f87b841bb7b2b7e9cb9
her17138 <her17138@uvg.edu.gt>
2018-03-02 22:30:20 -0600

    implementacion promedio

--- HT5.py
+++ HT5.py
@@ -5,75 +5,71 @@
 
 import simpy
 import random
+import math
 #import numpy as np
 #import statistics as stats
 
 
-Tiempoprocesos = [] #Lista para poder almacenar los tiempos de los procesos
+TiempoProcesos = [] #Lista para poder almacenar los tiempos de los procesos
 class Computer:
     def __init__(self, env):
-        self.RAM = simpy.Container(env, init = 0, capacity =100)
+        self.RAM = simpy.Container(env, init = 0, capacity =50)
         self.CPU = simpy.Resource(env, capacity =1)
         
 def proceso(env, name, numero, arriving_time, computadora):
-    #
+    
     yield env.timeout(arriving_time)
     #Momento en el que llega 
     horaLlegada = env.now
-    print "Proceso %s solicita memoria en el momento %s" % (name, env.now)
+    print "Proceso %s solicita memoria en tiempo =  %s" % (name, env.now)
     cantidadMemoria = random.randint(1, 30)
     print "Proceso %s necesita %d cantidad de memoria." % (name, cantidadMemoria)
     yield computadora.RAM.put(cantidadMemoria)
-    print "Memoria otorgada al momento %s al proceso %s. Ha pasado al estado 'Ready' en %s." % (env.now, name, env.now)
+    print "Memoria otorgada al proceso %s. Ha pasado al estado 'Ready' en tiempo = %s." % (name, env.now)
 
     terminado = False
     while not terminado:
         with computadora.CPU.request() as req:
             waiting =0
             #Momento para espere a que lo atienda el CPU
-            print "Espera al CPU el proceso %s con %d instrucciones en %s"% (name,cantidadMemoria,env.now)
+            print "Espera al CPU el proceso %s con %d instrucciones en tiempo = %s"% (name,cantidadMemoria,env.now)
             yield req
-            if instrucciones>3:
+            if cantidadMemoria > 0: #Si el proceso todavia tiene instrucciones por realizar     
                 for i in range(3): #Realiza solamente 3 instrucciones
-                    if instrucciones > 0:   #Si llegara a tener operaciones, llega a ejecutar una
-                        instrucciones -=1
-                        cantidadMemoria -=1
-                waiting = random.randint(1,2)
+                    cantidadMemoria -=1
+                    computadora.RAM.get(1)
+
+                waiting = random.randint(0,2)
                 yield env.timeout(1)
             #Finaliza de estar en la CPU
-            #Ingresa a cola de Waiting
-            if waiting == 1:
-                print "Proceso %s en cola de Waiting para hacer operaciones I/O en %s"% (name,env.now)
+            #Ingresa a cola de Waiting, si random ==0, pasa a cola ready
+            if (waiting ==0):
+                print "Proceso %s en cola de Ready en %s"% (name,env.now)
+            elif waiting == 1:
+                print "Proceso %s en cola de Waiting para hacer operaciones I/O en tiempo =  %s"% (name,env.now)
                 yield env.timeout(2)
             #Deja el CPU
-            if instrucciones <= 0:
-                computadora.RAM.get(cantidadMemoria)
-            if cantidadMemoria <3:
+            if cantidadMemoria <3 :
+                if cantidadMemoria > 0:
+                    computadora.RAM.get(cantidadMemoria)
                 terminado = True
-
-    print "Proceso %s ha dejado el CPU en %s"% (name,env.now)
+    
+    print "Proceso %s ha dejado el CPU en tiempo = %s"% (name,env.now)
+    print "MEMORIA RESTANTE: %d" % (computadora.RAM.capacity - computadora.RAM.level) +'\n'
     fin = env.now
-    tiempototal = horaLlegada - fin
-    Tiempoprocesos.insert(numero,tiempototal) #Para almacenar los tiempos de los procesos   
-            
-                    
-            
-
-
-
+    tiempototal =  fin - horaLlegada
+    TiempoProcesos.insert(numero,tiempototal) #Para almacenar los tiempos de los procesos   
+        
 
 # ----------------------
 
 env = simpy.Environment() #ambiente de simulacion
-#memoria = simpy.Container(env,init =0,capacity = 100)
-#CPU = simpy.Resource(env, capacity = 1)
 random.seed(10)
 compu = Computer(env)
-for i in range(2):
-    env.process(proceso(env,'%d'%i,i,random.expovariate(1.0/10), compu))
-env.run(50)  #correr la simulacionn hasta el tiempo = 50
-tiempopromedio = sum(Tiempoprocesos)*1.0/len(Tiempoprocesos)
-#desvestandar = np.std(Tiempoprocesos)
-#desvestandar = stats.pstdev(Tiempoprocesos)
-
-#print ("tiempo promedio por vehiculo es: ", totalDia/5.0)
+cantidadProcesos = 5
+for i in range(cantidadProcesos):
+    env.process(proceso(env, '%d'%i, i+1, random.expovariate(1.0/10), compu))
+    cantidadProcesos -=1
+env.run(until = None)  #correr la simulacion hasta que ya no hayan procesos
+tiempoPromedio = sum(TiempoProcesos)*1.0/len(TiempoProcesos)
+print "\n \n \t ---> TIEMPO PROMEDIO PARA CADA PROCESO: "+ str(tiempoPromedio)
